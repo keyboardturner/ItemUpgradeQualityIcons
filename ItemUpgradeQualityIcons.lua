b20532343ac487a1a4f56a3d5d06b1c1aa17b029
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2023-05-22 11:32:49 -0700

    ilvl max value, code cleanup
    updates & code cleanup, added item level maximum value to item level field for specific upgradeable items. An item that displayed "Item Level 411" will now directly display "Item Level 411/424"

--- ItemUpgradeQualityIcons.lua
+++ ItemUpgradeQualityIcons.lua
@@ -1,13 +1,11 @@
 -- Turning global string into pattern to match
-local patternToMatch = ITEM_UPGRADE_TOOLTIP_FORMAT_STRING
-patternToMatch = patternToMatch:gsub("%%s", ")(.*)(")
-patternToMatch = patternToMatch:gsub("%%d", "[0-9]+")
-patternToMatch = "(" .. patternToMatch .. ")"
+local patternUpgradeLevel = ITEM_UPGRADE_TOOLTIP_FORMAT_STRING
+patternUpgradeLevel = patternUpgradeLevel:gsub("%%s", ")(.*)(")
+patternUpgradeLevel = patternUpgradeLevel:gsub("%%d", "[0-9]+")
+patternUpgradeLevel = "(" .. patternUpgradeLevel .. ")"
 
-local patternIlvlMatch = ITEM_UPGRADE_ITEM_LEVEL_STAT_FORMAT
-patternIlvlMatch = patternIlvlMatch:gsub("%%s", ")(.*)(")
-patternIlvlMatch = patternIlvlMatch:gsub("%%d", "[0-9]+")
-patternIlvlMatch = "(" .. patternIlvlMatch .. ")"
+local patternIlvl = ITEM_UPGRADE_ITEM_LEVEL_STAT_FORMAT
+patternIlvl = "^" .. patternIlvl:gsub("%%d", "[0-9]+") .. "$"
 
 local LOCALE = GetLocale()
 
@@ -21,202 +19,193 @@ local categoryEnum = {
 
 if LOCALE == "enUS" then
 
--- Name keys (should have a way to localize keys)
-categoryEnum = {
-	Explorer = "Explorer",
-	Adventurer = "Adventurer",
-	Veteran = "Veteran",
-	Champion = "Champion",
-	Hero = "Hero",
-};
+	-- Name keys (should have a way to localize keys)
+	categoryEnum = {
+		Explorer = "Explorer",
+		Adventurer = "Adventurer",
+		Veteran = "Veteran",
+		Champion = "Champion",
+		Hero = "Hero",
+	};
 
 elseif LOCALE == "deDE" then
 
-categoryEnum = {
-	Explorer = "Forscher",
-	Adventurer = "Abenteurer",
-	Veteran = "Veteran",
-	Champion = "Champion",
-	Hero = "Held",
-};
+	categoryEnum = {
+		Explorer = "Forscher",
+		Adventurer = "Abenteurer",
+		Veteran = "Veteran",
+		Champion = "Champion",
+		Hero = "Held",
+	};
 
 elseif LOCALE == "esES" then
 
-categoryEnum = {
-	Explorer = "Expedicionario",
-	Adventurer = "Aventurero",
-	Veteran = "Veterano",
-	Champion = "Campeón",
-	Hero = "Héroe",
-};
+	categoryEnum = {
+		Explorer = "Expedicionario",
+		Adventurer = "Aventurero",
+		Veteran = "Veterano",
+		Champion = "Campeón",
+		Hero = "Héroe",
+	};
 
 elseif LOCALE == "frFR" then
 
-categoryEnum = {
-	Explorer = "Explorateur",
-	Adventurer = "Aventurier",
-	Veteran = "Vétéran",
-	Champion = "Champion",
-	Hero = "Héros",
-};
+	categoryEnum = {
+		Explorer = "Explorateur",
+		Adventurer = "Aventurier",
+		Veteran = "Vétéran",
+		Champion = "Champion",
+		Hero = "Héros",
+	};
 
 elseif LOCALE == "itIT" then
 
-categoryEnum = {
-	Explorer = "Esploratore",
-	Adventurer = "Avventuriero",
-	Veteran = "Veterano",
-	Champion = "Campione",
-	Hero = "Eroe",
-};
+	categoryEnum = {
+		Explorer = "Esploratore",
+		Adventurer = "Avventuriero",
+		Veteran = "Veterano",
+		Champion = "Campione",
+		Hero = "Eroe",
+	};
 
 elseif LOCALE == "ptBR" then
 
-categoryEnum = {
-	Explorer = "Explorador",
-	Adventurer = "Aventureiro",
-	Veteran = "Veterano",
-	Champion = "Campeão",
-	Hero = "Herói",
-};
+	categoryEnum = {
+		Explorer = "Explorador",
+		Adventurer = "Aventureiro",
+		Veteran = "Veterano",
+		Champion = "Campeão",
+		Hero = "Herói",
+	};
 
 elseif LOCALE == "ruRU" then
 
-categoryEnum = {
-	Explorer = "Исследователь",
-	Adventurer = "Искатель приключений",
-	Veteran = "Ветеран",
-	Champion = "Защитник",
-	Hero = "Герой",
-};
+	categoryEnum = {
+		Explorer = "Исследователь",
+		Adventurer = "Искатель приключений",
+		Veteran = "Ветеран",
+		Champion = "Защитник",
+		Hero = "Герой",
+	};
 
 elseif LOCALE == "koKR" then
 
-categoryEnum = {
-	Explorer = "탐험가",
-	Adventurer = "모험가",
-	Veteran = "노련가",
-	Champion = "챔피언",
-	Hero = "영웅",
-};
+	categoryEnum = {
+		Explorer = "탐험가",
+		Adventurer = "모험가",
+		Veteran = "노련가",
+		Champion = "챔피언",
+		Hero = "영웅",
+	};
 
 elseif LOCALE == "zhCN" then
 
-categoryEnum = {
-	Explorer = "探索者",
-	Adventurer = "冒险者",
-	Veteran = "老兵",
-	Champion = "勇士",
-	Hero = "英雄",
-};
+	categoryEnum = {
+		Explorer = "探索者",
+		Adventurer = "冒险者",
+		Veteran = "老兵",
+		Champion = "勇士",
+		Hero = "英雄",
+	};
 
 elseif LOCALE == "zhTW" then
 
-categoryEnum = {
-	Explorer = "探索者",
-	Adventurer = "冒险者",
-	Veteran = "老兵",
-	Champion = "勇士",
-	Hero = "英雄",
-};
+	categoryEnum = {
+		Explorer = "探索者",
+		Adventurer = "冒险者",
+		Veteran = "老兵",
+		Champion = "勇士",
+		Hero = "英雄",
+	};
 
 end
 
 
--- Item level ranges
-
-local itemLevelRanges = {
-	Explorer = {minLevel = 376, maxLevel = 398, color = ITEM_POOR_COLOR},
-	Adventurer = {minLevel = 389, maxLevel = 411, color = WHITE_FONT_COLOR},
-	Veteran = {minLevel = 402, maxLevel = 424, color = UNCOMMON_GREEN_COLOR},
-	Champion = {minLevel = 415, maxLevel = 437, color = RARE_BLUE_COLOR},
-	Hero = {minLevel = 428, maxLevel = 441, color = ITEM_EPIC_COLOR},
-	Mythic = {minLevel = 441, maxLevel = 450, color = ITEM_LEGENDARY_COLOR},
+-- Item category data
+local categoryDataTab = {
+	[categoryEnum.Explorer] = {minLevel = 376, maxLevel = 398, color = ITEM_POOR_COLOR, icon = "|A:Professions-ChatIcon-Quality-Tier1:20:20|a "},
+	[categoryEnum.Adventurer] = {minLevel = 389, maxLevel = 411, color = WHITE_FONT_COLOR, icon = "|A:Professions-ChatIcon-Quality-Tier2:20:20|a "},
+	[categoryEnum.Veteran] = {minLevel = 402, maxLevel = 424, color = UNCOMMON_GREEN_COLOR, icon = "|A:Professions-ChatIcon-Quality-Tier3:20:20|a "},
+	[categoryEnum.Champion] = {minLevel = 415, maxLevel = 437, color = RARE_BLUE_COLOR, icon = "|A:Professions-ChatIcon-Quality-Tier4:20:20|a "},
+	[categoryEnum.Hero] = {minLevel = 428, maxLevel = 441, color = ITEM_EPIC_COLOR, icon = "|A:Professions-ChatIcon-Quality-Tier5:20:20|a "},
 }
 
+local function SearchAndReplaceTooltipLine(tooltip, category)
+	-- Editing the upgrade line (need to do first because of fallback detection)
+	local categoryData;
 
--- Name to atlas array 
-local categoryStringToAdd = {
-	[categoryEnum.Explorer] = "|A:Professions-ChatIcon-Quality-Tier1:20:20|a ",
-	[categoryEnum.Adventurer] = "|A:Professions-ChatIcon-Quality-Tier2:20:20|a ",
-	[categoryEnum.Veteran] = "|A:Professions-ChatIcon-Quality-Tier3:20:20|a ",
-	[categoryEnum.Champion] = "|A:Professions-ChatIcon-Quality-Tier4:20:20|a ",
-	[categoryEnum.Hero] = "|A:Professions-ChatIcon-Quality-Tier5:20:20|a ",
-};
-
-local function SearchAndReplaceTooltipLine(tooltip, stringToAdd)
 	for i = 1, tooltip:NumLines() do
 		local line = _G[tooltip:GetName().."TextLeft"..i]
 		local text = line:GetText()
 		
-		if text and text:match(patternToMatch) then
-			local beforeText, categoryText, afterText = text:match(patternToMatch)
-		
-			-- No string = fallback method
-			if not stringToAdd then
-				stringToAdd = categoryStringToAdd[categoryText]
+		if text and text:match(patternUpgradeLevel) then
+			-- No category = fallback method
+			if not category then
+				local beforeText, afterText
+				beforeText, category, afterText = text:match(patternUpgradeLevel)
 			end
-		
-			-- Replacing the line
-			if stringToAdd then
-				text = text:gsub(patternToMatch, "%1" .. stringToAdd .. "%2%3")
-			end
-			
-			line:SetText(text)
-			line:Show()
-		end
 
-		if text and text:match(patternIlvlMatch) then
-			local beforeText, categoryText, afterText = text:match(patternIlvlMatch)
-		
-			-- No string = fallback method
-			if not ilvlStringToAdd then
-				ilvlStringToAdd = categoryStringToAdd[categoryText]
-			end
-		
+			categoryData = category and categoryDataTab[category]
+
+			if not categoryData then return end -- Invalid/non-existent category
+
 			-- Replacing the line
-			if ilvlStringToAdd then
-				text = text:gsub(patternIlvlMatch, "%1" .. ilvlStringToAdd)
-			end
+			text = text:gsub(patternUpgradeLevel, "%1" .. categoryData.icon .. "%2%3")
 			
 			line:SetText(text)
 			line:Show()
+			
+			break
+		end
+	end
+
+	-- Invalid/non-existent category
+	if not categoryData then
+		return
+	end
+	
+	-- Editing the ilvl line
+	for i = 1, tooltip:NumLines() do
+		local line = _G[tooltip:GetName().."TextLeft"..i]
+		local text = line:GetText()
+
+		if text and text:match(patternIlvl) then
+			text = text .. "/" .. categoryData.maxLevel
+
+			line:SetText(text)
+			line:Show()
+
+			break
 		end
 	end
 end
 
-TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(tooltip, data)
-	local itemName, itemLink = TooltipUtil.GetDisplayedItem(tooltip)
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, function(tooltip)
+	local _, itemLink = TooltipUtil.GetDisplayedItem(tooltip)
 	if not itemLink then return end
 
 	local itemLinkValues = StringSplitIntoTable(":", itemLink)
 	local numBonusIDs = tonumber(itemLinkValues[14])
 
 	if not numBonusIDs then return end
-	local stringToAdd;
+	local category;
 	for i = 1, numBonusIDs do
 		local upgradeID = tonumber(itemLinkValues[14 + i])
-		local colorCode = "|c" ..ITEM_POOR_COLOR:GenerateHexColor()
 		if upgradeID >= 9294 and upgradeID <= 9301 then
-			stringToAdd = categoryStringToAdd[categoryEnum.Explorer] -- Stuff to add before Explorer
-			ilvlStringToAdd = colorCode .. " (" .. itemLevelRanges.Explorer.minLevel .. " - " .. itemLevelRanges.Explorer.maxLevel .. ")|r"
+			category = categoryEnum.Explorer
 		elseif upgradeID >= 9301 and upgradeID <= 9309 then
-			stringToAdd = categoryStringToAdd[categoryEnum.Adventurer] -- Stuff to add before Adventurer
-			ilvlStringToAdd = colorCode .. " (" .. itemLevelRanges.Adventurer.minLevel .. " - " .. itemLevelRanges.Adventurer.maxLevel .. ")|r"
+			category = categoryEnum.Adventurer
 		elseif upgradeID >= 9313 and upgradeID <= 9320 then
-			stringToAdd = categoryStringToAdd[categoryEnum.Veteran] -- Stuff to add before Veteran
-			ilvlStringToAdd = colorCode .. " (" .. itemLevelRanges.Veteran.minLevel .. " - " .. itemLevelRanges.Veteran.maxLevel .. ")|r"
+			category = categoryEnum.Veteran
 		elseif upgradeID >= 9321 and upgradeID <= 9329 then
-			stringToAdd = categoryStringToAdd[categoryEnum.Champion] -- Stuff to add before Champion
-			ilvlStringToAdd = colorCode .. " (" .. itemLevelRanges.Champion.minLevel .. " - " .. itemLevelRanges.Champion.maxLevel .. ")|r"
+			category = categoryEnum.Champion
 		elseif upgradeID >= 9330 and upgradeID <= 9334 then
-			stringToAdd = categoryStringToAdd[categoryEnum.Hero] -- Stuff to add before Hero
-			ilvlStringToAdd = colorCode .. " (" .. itemLevelRanges.Hero.minLevel .. " - " .. itemLevelRanges.Hero.maxLevel .. ")|r"
+			category = categoryEnum.Hero
 		end
 	end
 
 	-- Searching the line
-	SearchAndReplaceTooltipLine(tooltip)
+	SearchAndReplaceTooltipLine(tooltip) -- (tooltip, category) once Blizz fixes the stale tooltips issue
 	
 	-- Compare tooltips
 	if tooltip.shoppingTooltips then
